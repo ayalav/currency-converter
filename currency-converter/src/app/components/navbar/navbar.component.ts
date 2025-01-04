321510acdabedb7a768baf4cf747109ac0e2dbea
ayalav <apap90948@gmail.com>
2025-01-05 00:28:43 +0200

    feat: redesign navbar using Angular Material

--- currency-converter/src/app/components/navbar/navbar.component.ts
+++ currency-converter/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
