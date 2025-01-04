60f376b7f36fc66f8666f134dec9643d2029927b
ayalav <apap90948@gmail.com>
2025-01-05 00:44:04 +0200

    feat: upgrade history table to use Angular Material

--- currency-converter/src/app/components/history/history.component.ts
+++ currency-converter/src/app/components/history/history.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { CurrencyService } from '../../services/currency.service';
 
 @Component({
   selector: 'app-history',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './history.component.html',
   styleUrl: './history.component.scss'
 })
 export class HistoryComponent {
+  displayedColumns: string[] = ['from', 'to', 'amount', 'result'];
+  
   constructor(public currencyService: CurrencyService) {}
 }
