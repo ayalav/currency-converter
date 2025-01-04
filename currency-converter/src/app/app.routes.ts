b1737781a080ff329b43b81ea4f9bad030aa6eea
ayalav <apap90948@gmail.com>
2025-01-04 23:46:39 +0200

    feat: add ChartComponent to application routes

--- currency-converter/src/app/app.routes.ts
+++ currency-converter/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ConverterComponent } from './components/converter/converter.component';
 import { HistoryComponent } from './components/history/history.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 export const routes: Routes = [
     { path: '', component: ConverterComponent },
     { path: 'history', component: HistoryComponent },
+    { path: 'chart', component: ChartComponent },
 ];
